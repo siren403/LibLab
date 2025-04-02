b4bb9687cafc8a35ed7b8d980b082e29e326b678
aaron <qkrsogusl3@gmail.com>
2025-04-02 17:57:07 +0900

    feat(Unity): Check addressable exceptions

--- LibLab.Unity/Assets/App/Scripts/Runtime/AddressableRouterEntry.cs
+++ LibLab.Unity/Assets/App/Scripts/Runtime/AddressableRouterEntry.cs
@@ -5,33 +5,71 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.Exceptions;
 using UnityEngine.ResourceManagement.ResourceLocations;
 using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
 using VContainer.Unity;
+using ZLogger;
 
 namespace App
 {
     public class AddressableRouterEntry : IAsyncStartable
     {
-        public async UniTask StartAsync(CancellationToken cancellation = new CancellationToken())
+        private readonly ILogger<AddressableRouterEntry> _logger;
+
+        public AddressableRouterEntry(ILogger<AddressableRouterEntry> logger)
         {
-            await Addressables.InitializeAsync();
-            await Addressables.CheckForCatalogUpdates();
+            _logger = logger;
+        }
 
-            var firstScene = SceneManager.GetSceneAt(0);
-            if (firstScene.buildIndex != 0) return;
+        public async UniTask StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var initResult = await AddressableExtensions.Initialize();
+                _logger.ZLogInformation($"Addressable Initialized: {initResult.Result}");
+                if (_logger.IsEnabled(LogLevel.Debug))
+                {
+                    foreach (IResourceLocation location in initResult.Result.AllLocations)
+                    {
+                        _logger.ZLogDebug($"Addressable Location: {location}");
+                    }
+                }
 
+                // Try enter root route
+                var firstScene = SceneManager.GetSceneAt(0);
+                if (firstScene.buildIndex != 0)
+                {
+                    return;
+                }
+
+                // Catalog updates
+
+                await Push("/");
+
+                await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: cancellationToken);
+                await AddressableExtensions.CheckForCatalogUpdates();
+                await Push("/intro");
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e.Message);
+            }
+        }
+
+        private async UniTask Push(string path)
+        {
             IList<IResourceLocation> locations =
-                await Addressables.LoadResourceLocationsAsync("/").Task.AsUniTask();
+                await Addressables.LoadResourceLocationsAsync(path).Task;
 
             HashSet<string> loadedScenes = new();
             for (int i = 0; i < SceneManager.sceneCount; i++)
             {
-                Debug.Log("Loaded Scene: " + SceneManager.GetSceneAt(i).path);
                 loadedScenes.Add(SceneManager.GetSceneAt(i).path);
             }
 
@@ -41,20 +79,18 @@ namespace App
             {
                 if (loadedScenes.Contains(location.ToString()))
                 {
-                    Debug.Log("Scene already loaded: " + location);
+                    _logger.ZLogDebug($"Scene already loaded: {location}");
                     continue;
                 }
-                string path = location.ToString();
-                Debug.Log("Loading scene: " + path);
+
+                string locationStr = location.ToString();
+                _logger.ZLogDebug($"Loading scene: {locationStr}");
                 AsyncOperationHandle<SceneInstance> handle = Addressables.LoadSceneAsync(location,
                     LoadSceneMode.Additive,
                     SceneReleaseMode.ReleaseSceneWhenSceneUnloaded, false);
-                handle.Destroyed += operationHandle =>
-                {
-                    Debug.Log("Scene destroyed: " + path);
-                };
+                // handle.Destroyed += operationHandle => { Debug.Log("Scene destroyed: " + locationStr); };
                 sceneHandles.Add(handle);
-                sceneLoadTasks.Add(handle.Task.AsUniTask());
+                sceneLoadTasks.Add(handle.ToUniTask());
             }
 
             await UniTask.WhenAll(sceneLoadTasks);
@@ -64,7 +100,7 @@ namespace App
                 await sceneHandle.Result.ActivateAsync();
             }
 
-            Debug.Log("All scenes loaded");
+            _logger.ZLogInformation($"Load completed: {path}");
         }
     }
 }
---
+++ LibLab.Unity/Assets/App/Scripts/Runtime/Scenes/ZLoggerExtensions.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Microsoft.Extensions.Logging;
+using UnityEngine;
+using VContainer;
+using ZLogger;
+using ZLogger.Formatters;
+
+namespace App.Scenes;
+
+public static class ZLoggerExtensions
+{
+    public static void RegisterZLogger(this IContainerBuilder builder, Action<ILoggingBuilder> configure)
+    {
+        var loggerFactory = LoggerFactory.Create(configure);
+        builder.RegisterInstance(loggerFactory);
+        builder.Register(typeof(Logger<>), Lifetime.Singleton).As(typeof(ILogger<>));
+
+        var appLogger = loggerFactory.CreateLogger<Application>();
+        builder.RegisterInstance(appLogger);
+        // Application.logMessageReceived += onApplicationLog;
+        builder.RegisterDisposeCallback(_ =>
+        {
+            loggerFactory.Dispose();
+            // Application.logMessageReceived -= onApplicationLog;
+        });
+
+        // void onApplicationLog(string message, string stackTrace, LogType type)
+        // {
+        //     switch (type)
+        //     {
+        //         case LogType.Exception:
+        //         case LogType.Error:
+        //             appLogger.LogCritical(message);
+        //             break;
+        //     }
+        // }
+    }
+
+    public static void WithEditorConsolePro(this PlainTextZLoggerFormatter formatter)
+    {
+        formatter.SetPrefixFormatter($"{0} | {1:short} | {2} | ",
+            (in MessageTemplate template, in LogInfo info) =>
+                template.Format(info.Timestamp, info.LogLevel, info.Category));
+    }
+}
